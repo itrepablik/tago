522be91710f7e8e22e699016c9d7314a97c0f952
Pol Peligro <itrepablik@gmail.com>
2021-12-09 17:05:45 +0800

    Update tago_test.go

--- tago_test.go
+++ tago_test.go
@@ -9,14 +9,12 @@ func TestEncrypt(t *testing.T) {
 	iv, err := GenerateIV()
 	if err != nil {
 		t.Fatalf("error generating IV: %s", err)
-		return
 	}
 
 	// Generate a secure random salt
 	secretKey, err := GenerateSecretKey(32)
 	if err != nil {
 		t.Fatalf("error generating secret key: %s", err)
-		return
 	}
 
 	// Encrypt a string
@@ -37,32 +35,31 @@ func TestDecrypt(t *testing.T) {
 	iv, err := GenerateIV()
 	if err != nil {
 		t.Fatalf("error generating IV: %s", err)
-		return
 	}
 
 	// Generate a secure random salt
 	secretKey, err := GenerateSecretKey(32)
 	if err != nil {
-		t.Errorf("error generating secret key: %s", err)
+		t.Fatalf("error generating secret key: %s", err)
 	}
 
 	// Encrypt a string
 	plaintext := "Hello World!"
 	ciphertext, err := Encrypt(plaintext, string(secretKey), iv)
 	if err != nil {
-		t.Errorf("error decrypting string: %s", err)
+		t.Fatalf("error encrypting string: %s", err)
 	}
 	if ciphertext == plaintext {
-		t.Errorf("plaintext and ciphertext should not be the same")
+		t.Fatalf("plaintext and ciphertext should not be the same")
 	}
 
 	// Test to Decrypt back to original plaintext
 	decrypted, err := Decrypt(ciphertext, string(secretKey), iv)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("error decrypting string: %s", err)
 	}
 	if decrypted != plaintext {
-		t.Error("decrypt failed")
+		t.Fatalf("decrypted string should be the same as plaintext")
 	}
 	t.Logf("plaintext: %s\nciphertext: %s\ndecrypted: %s", plaintext, ciphertext, decrypted)
 }
@@ -71,10 +68,10 @@ func TestGenerateSecretKey(t *testing.T) {
 	// Generate a secure random salt
 	secretKey, err := GenerateSecretKey(32)
 	if err != nil {
-		t.Errorf("error generating secret key: %s", err)
+		t.Fatalf("error generating secret key: %s", err)
 	}
 	if len(secretKey) != 32 {
-		t.Errorf("secret key length should be 32, got %d", len(secretKey))
+		t.Fatalf("secret key length should be 32, got %d", len(secretKey))
 	}
 	t.Logf("secret key: %s", secretKey)
 }
@@ -83,10 +80,10 @@ func TestGenerateIV(t *testing.T) {
 	// Generate a secure random IV
 	iv, err := GenerateIV()
 	if err != nil {
-		t.Errorf("error generating IV: %s", err)
+		t.Fatalf("error generating IV: %s", err)
 	}
 	if len(iv) != 16 {
-		t.Errorf("IV length should be 16, got %d", len(iv))
+		t.Fatalf("IV length should be 16, got %d", len(iv))
 	}
 	t.Logf("IV: %s", string(iv))
 }
